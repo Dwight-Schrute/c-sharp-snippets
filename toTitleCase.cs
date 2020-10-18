63ebbdd9df364b1bbb696358097581ef203d8443
Dwight Kurt Schrute <dschrute@protonmail.com>
2020-10-18 16:49:46 -0400

    Create toTitleCase.cs

---
+++ toTitleCase.cs
@@ -0,0 +1,16 @@
+//without declaring namespace directives like "using System;", "using System.Threading;".
+
+public class Program
+{
+	public static void Main()
+	{
+				
+		string clp = System.Windows.Forms.Clipboard.GetText();
+
+		string result = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(clp.ToLower());
+
+		System.Windows.Forms.Clipboard.SetText(result);
+
+		//System.Windows.Forms.MessageBox.Show(result);
+	}
+}

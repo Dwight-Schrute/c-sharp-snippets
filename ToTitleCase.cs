4172860b0e290b59eee50078726bf281f5c51c0b
Dwight Kurt Schrute <dschrute@protonmail.com>
2020-10-18 20:17:57 -0400

    Update ToTitleCase.cs

--- ToTitleCase.cs
+++ ToTitleCase.cs
@@ -1,19 +1,11 @@
 //without declaring namespace directives like "using System;", "using System.Threading;" as they are invoked directly.
-//using System.Windows.Forms;
-//using System.Threading;
-
-
 public class Program
 {
 	public static void Main()
-	{
-				
+	{		
 		string clp = System.Windows.Forms.Clipboard.GetText();
-
 		string result = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(clp.ToLower());
-
 		System.Windows.Forms.Clipboard.SetText(result);
-
-		//System.Windows.Forms.MessageBox.Show(result);
 	}
 }
+//System.Windows.Forms.MessageBox.Show(result);

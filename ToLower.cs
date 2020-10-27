8986ec28c0e6d8eb35de06ee431d104a1b214ed5
Dwight Kurt Schrute <dschrute@protonmail.com>
2020-10-27 12:08:03 -0400

    added if stmnt to remove first parenthesis.

--- ToLower.cs
+++ ToLower.cs
@@ -3,8 +3,16 @@ public class Program
     public static void Main()
     {
         string clp = System.Windows.Forms.Clipboard.GetText();
-        int s1 = clp.IndexOf("(");
-        string result = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToLower(clp);
-        System.Windows.Forms.Clipboard.SetText(result.Remove(s1).Trim());
+        if (clp.Contains("("))
+        {
+            int s1 = clp.IndexOf("(");
+            string result = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToLower(clp);
+            System.Windows.Forms.Clipboard.SetText(result.Remove(s1).Trim());
+        }
+        else
+        {
+            string result = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToLower(clp);
+            System.Windows.Forms.Clipboard.SetText(result.Trim());
+        }
     }
 }
